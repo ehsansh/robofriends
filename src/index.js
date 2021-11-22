d7d4ff9673fc2eaa27f6d210624e76979100a7a3
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2021-11-22 19:37:58 +0330

    start the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello';
+
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello greeting = { 'hello my friends' } />
   </React.StrictMode>,
   document.getElementById('root')
 );
