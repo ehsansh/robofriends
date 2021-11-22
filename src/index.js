6f2c1cc163eee9e3696b426e325a02bbb9739903
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2021-11-22 19:55:51 +0330

    add more cards and some style to the cards

--- src/index.js
+++ src/index.js
@@ -9,11 +9,10 @@ import 'tachyons';
 ReactDOM.render(
   <React.StrictMode>
     <Card />
+    <Card />
+    <Card />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
