970e46902bc50da1f9a998b6b4e03edf395fde60
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2021-11-22 23:54:55 +0330

    add api to App.js to fetch robots from a server

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from './robots';
+
 
 class App extends Component{
     constructor(){
         super()
         this.state = {
-            robots : robots,
+            robots : [],
             searchfield:''
         }
     }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then( response => response.json() )
+            .then( users => { this.setState({ robots:users }) } )
+    }
+
     onSearchChange = (event) =>{
         this.setState({ searchfield: event.target.value });
-
     }
     render(){
         const filteredRobots = this.state.robots.filter( robot => {
             return robot.name.toLowerCase().includes( this.state.searchfield.toLowerCase() )
         } )
-        return(
+        if( this.state.robots.length === 0 ){
+            return( <h1>Loading...</h1> )
+        }else{
+            return(
+                <div className="tc">
+                    <h1>Robo friends</h1>
+                    <SearchBox searchChange={ this.onSearchChange }/>
+                    <CardList robots={ filteredRobots }  />
+                </div>
+            )
+        }
 
-            <div className="tc">
-                <h1>Robo friends</h1>
-                <SearchBox searchChange={ this.onSearchChange }/>
-                <CardList robots={ filteredRobots }  />
-            </div>
-            
-        )
     }
 }
 
