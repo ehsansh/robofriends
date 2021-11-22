17934f7a02e700b2e9dbf8f3ad8603b152e3cdea
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2021-11-22 19:59:22 +0330

    add robots.js file and edit card.js

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return(
+        <div className="bg-light-green dib br3 pad3 ma2 grow bw2 shadow-5">
+            <img alt="" src="https://robohash.org/test>200x200" />
+            <h2>John Doe</h2>
+            <p>
+                john.doe@gmail.com
+            </p>
+        </div>
+    )
+}
+
+export default Card;
